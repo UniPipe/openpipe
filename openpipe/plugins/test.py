519a5f7455e6f5825309281a8d626f33ef8f964a
João Pinto <jpinto@ccsint.eu>
2019-02-18 18:42:07 +0000

    Added the test plugin

--- openpipe/plugins/test.py
+++ openpipe/plugins/test.py
@@ -4,7 +4,10 @@ from openpipe.engine import PluginRuntime
 class Plugin(PluginRuntime):
 
     def on_start(self, config, segment_resolver):
-        pass
+        self.send_to_target = segment_resolver(config['send_to'])
 
-    def on_input(self, config):
-        pass
+    def on_input(self, item):
+        if self.config['on_condition']:
+            self.put_target(item, self.send_to_target)
+        else:
+            self.put(item)
