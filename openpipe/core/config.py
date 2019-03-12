291d22764c7dc07e0ff9d13b4a85c4026120a9fc
João Pinto <lamego.pinto@gmail.com>
2019-03-12 17:38:59 +0000

    More work on the new config

--- openpipe/core/config.py
+++ openpipe/core/config.py
@@ -13,76 +13,59 @@ def check_missing(required_config, provided_config, required_config_str, plugin_
             exit(3)
 
 
-def validated_config(module_plugin, plugin_label, provided_config):  # NOQA: C901
-    """ Validate that the provided_config matches the plugin supported config format """
-    final_config = {}
-    valid_keys = []
+def validate_required_config(module_plugin, plugin_label, provided_config):
+    """ """
     required_config_str = getattr(module_plugin, 'required_config', None)
-    optional_config_str = getattr(module_plugin, 'optional_config', None)
+    if not required_config_str:
+        return None
 
-    required_config = load_yaml(required_config_str) if required_config_str else None
-    optional_config = load_yaml(optional_config_str) if optional_config_str else None
-    print("PROVCONFIG=", provided_config)
-    print("RCONFIG=", required_config)
-    print("OCONFIG=", optional_config)
+    required_config = load_yaml(required_config_str)
+    del required_config['__line__']
 
-    # Validate plugin configuration scheme
-    if isinstance(required_config, dict):
-        del required_config['__line__']
-        for key, value in required_config.items():
-            # Required config keys must not have a default value
-            print("VAL=", value)
-            assert(value is None)
+    # Required config must be a dict
+    assert(isinstance(required_config, dict))
 
-    if isinstance(optional_config, dict):
-        del optional_config['__line__']
-        # Default config must provide values
-        for key, value in optional_config.items():
-            assert(value is not None)
+    # all the _dict_ values must be set to None
+    for value in required_config.values():
+        assert(value is None)
 
-    if required_config:
-        valid_keys += required_config.keys()
-        # a single key is required and a non dict was provided
-        if not isinstance(provided_config, dict):
-            required_config_keys = list(required_config.keys())
-            if len(required_config_keys) == 1:
-                print("GOT TMP")
-                final_config[next(iter(required_config))] = provided_config
-                provided_config = {}    # To be merged with default config, if any
-            else:
-                print("Invalid configuration format for", plugin_label, file=stderr)
-                print("Got", type(provided_config), pformat(provided_config), file=stderr)
-                print("Expected dictionary with fields", required_config_str, file=stderr)
+    config = {}
+    required_config = load_yaml(required_config_str)
+
+    if isinstance(provided_config, dict):
+        for key in required_config:
+            try:
+                config[key] = provided_config[key]
+            except KeyError:
+                print("Invalid configuration for", plugin_label, file=stderr)
+                print("The required field '%s' is missing" % key, plugin_label, file=stderr)
+                print("The following configurations is required:", required_config_str, file=stderr)
                 exit(3)
+            del provided_config[key]
+    else:
+        if len(list(required_config.keys())) == 1:
+            config[next(iter(required_config))] = provided_config
+            provided_config = None
         else:
-            check_missing(required_config, provided_config, required_config_str, plugin_label)
+            print("Invalid configuration for", plugin_label, file=stderr)
+            print("Got", type(provided_config), pformat(provided_config), file=stderr)
+            print("Expected dictionary with fields", required_config_str, file=stderr)
+            exit(4)
+    return config
 
-    if optional_config:
-        if isinstance(optional_config, dict):
-            valid_keys += optional_config.keys()
-            if not isinstance(provided_config, dict):
-                print("Invalid configuration format for", plugin_label, file=stderr)
-                print("Got", type(provided_config), pformat(provided_config), file=stderr)
-                print("Expected dictionary with fields", optional_config_str, file=stderr)
-                exit(4)
-            final_config = {**optional_config, **provided_config}
-        else:
-            final_config = provided_config
+def validate_optional_config(required_config, module_plugin, plugin_label, provided_config):
+    """ validate optional config and return the complete config """
+    required_config_str = getattr(module_plugin, 'required_config', None)
+    if not required_config_str:
+        return None
 
-    if isinstance(provided_config, dict) and valid_keys:
-        for key, value in provided_config.items():
-            if key not in valid_keys:
-                print("Invalid configuration key for", plugin_label, file=stderr)
-                print("Unexpected key:\n", key, "=", value, file=stderr)
-                print("Avaiable configuration keys", optional_config_str, file=stderr)
-                exit(4)
-    print("RESCONFIG=", final_config)
-    if isinstance(final_config, dict) and isinstance(provided_config, dict):
-        assert(len(final_config) >= len(provided_config))
+    required_config = load_yaml(required_config_str)
+    del required_config['__line__']
 
-    if not valid_keys and optional_config is None and provided_config is not None:
-        print("Invalid configuration key for", plugin_label, file=stderr)
-        print("Unexpected configuration:\n", provided_config, file=stderr)
-        print("The plugin does not accept any configuration", file=stderr)
 
-    return final_config
+def validated_config(module_plugin, plugin_label, provided_config):
+    """ Validate that the provided_config is valid per the plugin config schema """
+    required_config = validate_required_config(module_plugin, plugin_label, provided_config)
+    config = validate_optional_config(required_config, module_plugin, plugin_label, provided_config)
+
+    return config
