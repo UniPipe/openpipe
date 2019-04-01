9bb48297f5bee40b7d8a36e31053492532154f84
João Pinto <lamego.pinto@gmail.com>
2019-04-01 17:10:34 +0100

    black fix

--- openpipe/tests/test_named_start_segment.py
+++ openpipe/tests/test_named_start_segment.py
@@ -3,8 +3,7 @@ from openpipe.cli.run import pipeline_run
 
 def test_default_start():
     exit_code, exit_message = pipeline_run(
-        "openpipe/tests/test_named_start_segment.yaml",
-        start_segment="good segment"
+        "openpipe/tests/test_named_start_segment.yaml", start_segment="good segment"
     )
     print(exit_code, exit_message)
     if exit_code != 0:
