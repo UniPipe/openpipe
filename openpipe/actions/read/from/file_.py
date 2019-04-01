6f2093be328d3e703d2131586c3fba325ba369f8
João Pinto <lamego.pinto@gmail.com>
2019-04-01 10:39:38 +0100

    url handling moved to it's own action

--- openpipe/actions/read/from/file_.py
+++ openpipe/actions/read/from/file_.py
@@ -4,12 +4,8 @@ Produce metadata/content from a local or remote file
 import gzip
 import bz2
 import lzma
-import zlib
 import mimetypes
-import urllib.request as urlreq
-from re import findall
-from urllib.error import HTTPError
-from io import BytesIO, StringIO
+from io import BytesIO
 from os.path import splitext, expanduser
 from openpipe.pipeline.engine import ActionRuntime
 
@@ -31,11 +27,6 @@ class Action(ActionRuntime):
 
     # The following option is only applicable to local filenames
     auto_expand_home: True      # Expand '~' on path to user home dir
-
-    # The following options are only relevant for HTTP/HTTPS/FTP paths
-    timeout: 30                 # Global timeout (in secs) for the operation
-    ignore_http_errors: False   # Ignore HTTP errors replies
-    user_agent: curl/7.64.0     # User-agent to use on HTTP requests
     """
 
     """
@@ -58,16 +49,6 @@ class Action(ActionRuntime):
 
     def on_input(self, item):
         path = self.config["path"]
-        schema = path.split(":", 1)[0]
-        is_remote = ":" in path and schema in ["http", "https", "ftp"]
-        mime_type = mimetypes.guess_type(path)[0]
-        if is_remote:
-            self.collect_remote_file(path, mime_type)
-        else:
-            self.collect_local_file(path, mime_type, item)
-
-    def collect_local_file(self, path, mime_type, item):
-
         ext_map = {
             ".gz": lambda x: gzip.open(x, "r"),
             ".bz2": lambda x: bz2.open(x, "r"),
@@ -86,6 +67,12 @@ class Action(ActionRuntime):
         if file_extension in ext_map:
             filename, file_extension = splitext(filename)
             mime_type = mimetypes.guess_type(path)[0]
+        else:
+            mime_type = mimetypes.guess_type(path)[0]
+
+        forced_mime_type = self.config.get("mime_type")
+        if forced_mime_type != "auto":
+            mime_type = forced_mime_type
 
         if path == "-":
             return self.decode(BytesIO(item), mime_type)
@@ -97,60 +84,12 @@ class Action(ActionRuntime):
                 mime_type = self.config["mime_type"]
             self.decode(file, mime_type)
 
-    def collect_remote_file(self, path, mime_type):
-
-        ext_map = {
-            ".gz": lambda x: zlib.decompress(x, 16 + zlib.MAX_WBITS),
-            ".xz": lambda x: lzma.decompress(x),
-            ".bz2": lambda x: bz2.decompress(x),
-        }
-        url = path
-        req = urlreq.Request(url)
-        req.add_header("User-Agent", self.config["user_agent"])
-        try:
-            reply = urlreq.urlopen(req, timeout=self.config["timeout"])
-        except HTTPError:
-            if self.config["ignore_http_errors"]:
-                return
-            raise
-        file_data = reply.read()
-        filename, file_extension = splitext(url)
-        mime_type, mime_extension = mimetypes.guess_type(path)
-        encoding = None
-        try:
-            content_type = reply.getheader("Content-Type")
-        except AttributeError:  # FTP does not have a getheader
-            pass
-        else:
-            # Only use header mime type when mime type was not determined by extension
-            if not mime_type:
-                mime_type = content_type.split(";", 1)[0]
-            charset = findall(r"charset=(\S*)", content_type)
-            if charset:
-                encoding = charset[0]
-
-        decompress_func = ext_map.get(file_extension)
-        if decompress_func:
-            file_data = decompress_func(file_data)
-            # If we got a gz file, guess the mime type from the remaining extension
-            filename, file_extension = splitext(filename)
-            mime_type = mimetypes.guess_type(path)[0]
-        if encoding:
-            file_data = file_data.decode(encoding)
-            file = StringIO(file_data)
-        else:
-            file = BytesIO(file_data)
-        self.decode(file, mime_type)
-
-    def decode(self, fileobj, mime_type):
-        forced_mime_type = self.config.get("mime_type")
-        if forced_mime_type != "auto":
-            mime_type = forced_mime_type
+    def decode(self, fileobj, mime_type, sender_action=None):
         if mime_type is None:
             mime_type = "text/plain"
         decoder_function = self.MIME_FILE_HANDLER.get(mime_type)
         if decoder_function:
-            decoder_function(fileobj, self)
+            decoder_function(fileobj, sender_action or self)
         else:
             data = fileobj.read()
             self.put(data)
---
+++ openpipe/actions/read/from/url_.py
@@ -0,0 +1,92 @@
+"""
+Produce metadata/content from an URL
+"""
+import bz2
+import lzma
+import zlib
+import mimetypes
+import urllib.request as urlreq
+from io import BytesIO, StringIO
+from re import findall
+from urllib.error import HTTPError
+from openpipe.pipeline.engine import ActionRuntime
+from os.path import splitext
+from .file_ import Action as FileAction
+
+
+class Action(ActionRuntime):
+
+    category = "Data Sourcing"
+
+    required_config = """
+    url:                       # HTTP/HTTPS/FTP url
+    """
+
+    optional_config = """
+    # The mime_type will be used by the action to identify and automatically
+    # decode the file content.
+    # With the default value of 'auto' the action will try to guess the
+    # mime type based on the content header or file extension.
+    mime_type:  auto
+
+    # The following options are only relevant for HTTP/HTTPS/FTP paths
+    timeout: 30                 # Global timeout (in secs) for the operation
+    ignore_http_errors: False   # Ignore HTTP errors replies
+    user_agent: curl/7.64.0     # User-agent to use on HTTP requests
+    """
+
+    def on_start(self, config):
+        self.extend(__file__, "_file")
+        self.file_action = FileAction(self.action_label, None)
+        self.file_action.extend(__file__, "_file")
+
+    def on_input(self, item):
+        url = self.config["url"]
+        mime_type = mimetypes.guess_type(url)[0]
+        ext_map = {
+            ".gz": lambda x: zlib.decompress(x, 16 + zlib.MAX_WBITS),
+            ".xz": lambda x: lzma.decompress(x),
+            ".bz2": lambda x: bz2.decompress(x),
+        }
+        req = urlreq.Request(url)
+        req.add_header("User-Agent", self.config["user_agent"])
+        try:
+            reply = urlreq.urlopen(req, timeout=self.config["timeout"])
+        except HTTPError:
+            if self.config["ignore_http_errors"]:
+                return
+            raise
+        file_data = reply.read()
+        mime_type = mimetypes.guess_type(url)[0]
+        print("MT", mime_type, url)
+        encoding = None
+        try:
+            content_type = reply.getheader("Content-Type")
+        except AttributeError:  # FTP does not have a getheader
+            pass
+        else:
+            # Only use header mime type when mime type was not determined by extension
+            if not mime_type:
+                mime_type = content_type.split(";", 1)[0]
+            charset = findall(r"charset=(\S*)", content_type)
+            if charset:
+                encoding = charset[0]
+
+        filename, file_extension = splitext(url)
+        decompress_func = ext_map.get(file_extension)
+        if decompress_func:
+            file_data = decompress_func(file_data)
+            # If we got a gz file, guess the mime type from the remaining extension
+            filename, file_extension = splitext(filename)
+            mime_type = mimetypes.guess_type(url)[0]
+        if encoding:
+            file_data = file_data.decode(encoding)
+            file = StringIO(file_data)
+        else:
+            file = BytesIO(file_data)
+
+        forced_mime_type = self.config.get("mime_type")
+        if forced_mime_type != "auto":
+            mime_type = forced_mime_type
+
+        self.file_action.decode(file, mime_type, self)
