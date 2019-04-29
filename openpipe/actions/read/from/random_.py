3c050be07086b9d2b0920a754bfc6aa225ac6fd8
João Pinto <lamego.pinto@gmail.com>
2019-04-29 16:56:17 +0100

    * Add action 'tag as' to use the input item as a tag key value (closes #22)

--- openpipe/actions/read/from/random_.py
+++ openpipe/actions/read/from/random_.py
@@ -3,6 +3,7 @@ Generate a random number
 """
 from openpipe.pipeline.engine import ActionRuntime
 from random import seed, randint
+from functools import partial
 
 
 class Action(ActionRuntime):
@@ -16,7 +17,29 @@ class Action(ActionRuntime):
 
     def on_start(self, config):
         seed()
+        if isinstance(config, dict):
+            random_dict = self.random_dict = {}
+            for key, value in config.items():
+                random_dict[key] = self.random_function(value)
+            self.on_input = self.on_input_dict
+        else:
+            self.random_func = self.random_function(config)
 
     # Output a random number
     def on_input(self, item):
-        self.put(randint(0, self.config))
+        self.put(self.random_func())
+
+    def on_input_dict(self, item):
+        new_item = {}
+        for key, random_func in self.random_dict.items():
+            new_item[key] = random_func()
+        self.put(new_item)
+
+    @staticmethod
+    def random_function(random_spec):
+        """ Return a function that provides the feature described by random_spec """
+        if isinstance(random_spec, int):
+            return partial(randint, 0, random_spec)
+        if isinstance(random_spec, str):
+            start, end = random_spec.split("..")
+            return partial(randint, int(start), int(end))
