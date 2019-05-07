480afdd52d0a1893f6b08ae176bf08c1efc3186b
João Pinto <lamego.pinto@gmail.com>
2019-05-07 17:16:27 +0100

    Improved the split action

--- openpipe/actions/split_.py
+++ openpipe/actions/split_.py
@@ -10,13 +10,20 @@ class Action(ActionRuntime):
 
     optional_config = """
         separator: " "          # String used to separate fields
-        max_splits: -1          # Maximum number of split operations
+        max_split: -1           # Maximum number of split operations
+        field_list: []          # List of keys to to be used for each value
     """
 
     def on_start(self, config):
         self.separator = config["separator"]
-        self.max_splits = config["max_splits"]
+        self.max_splits = config["max_split"]
+        self.field_list = config["field_list"]
 
     def on_input(self, item):
         new_item = item.split(self.separator, self.max_splits)
-        self.put(new_item)
+        if self.field_list:
+            new_dict_item = {}
+            for i, key in enumerate(self.field_list):
+                new_dict_item[key] = new_item[i]
+        else:
+            self.put(new_item)
